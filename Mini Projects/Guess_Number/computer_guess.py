f2404a03a0cbb730b185285bf7bd6cba54dd2028
Rishika <rishikasardana@email.com>
2025-08-29 17:44:19 -0700

    Computer Guesses

---
+++ Mini Projects/Guess_Number/computer_guess.py
@@ -0,0 +1,26 @@
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        
+        feedback = input(f"is {guess} too high(H), too low(L) or correct(C): ").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"Yay! Computer you guessed the number {guess} correctly!!")
+
+computer_guess(10)
+
+
+
+
